2849b5eefa65e0b208ec9ad0fd5f9ae32816fe54
AmmaarKhan03 <ikhanammaar@gmail.com>
2025-12-24 23:16:25 -0800

    adding databse

---
+++ src/Storage.cpp
@@ -0,0 +1,9 @@
+#include "Storage.h"
+#include "Auth.h"
+#include <string>
+
+bool loadUsers(UsersMap &users, const std::string &filename) {
+
+	std::ifstream(filename);
+
+}
---
+++ src/Storage.h
@@ -0,0 +1,7 @@
+#include <Auth.h>
+#include <string>
+
+
+bool loadUsers(UsersMap &users, const std::string &filename);
+bool saveUsers(const UsersMap &users, const std::string &filename);
+
