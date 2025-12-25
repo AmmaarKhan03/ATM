01c546d176143418e2230ed9fea59e5c3fe772b3
AmmaarKhan03 <ikhanammaar@gmail.com>
2025-12-24 23:48:48 -0800

    finished loadusers

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -1,9 +1,47 @@
 #include "Storage.h"
 #include "Auth.h"
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 bool loadUsers(UsersMap &users, const std::string &filename) {
 
-	std::ifstream(filename);
+	// ifstream is reading from a file
+	std::ifstream file(filename);  // tries to open file
 
+	if (!file.is_open()) { // checks if its opened
+		return true;
+	}
+
+
+	std::string line;
+	while (std::getline(file,line)) {  // gets line then moves to the next
+		std::stringstream ss(line);
+		std::string username, salt, hash;
+		std::getline(ss, username, '|');
+		std::getline(ss, salt, '|');
+		std::getline(ss, hash, '|');
+		User u;
+		u.username = username;
+		u.salt = salt;
+		u.passwordHash = hash;
+		users[username] = u;
+	}
+
+
+	return true;
+}
+
+
+bool saveUsers(const UsersMap &users, const std::string &filename) {
+
+	// writes to a file
+	std::ofstream file(filename);
+	if (!file.is_open()) {
+		return false;
+	}
+
+	for (users.first, us)
+	return true;
 }
