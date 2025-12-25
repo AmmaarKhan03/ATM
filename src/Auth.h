bef3dea64f5d5260427ebb043cd394fdf9b841e0
AmmaarKhan03 <ikhanammaar@gmail.com>
2025-12-24 21:45:24 -0800

    added auth prototypes

--- src/Auth.h
+++ src/Auth.h
@@ -1,4 +1,6 @@
+#pragma once
 #include <string>
+#include <unordered_map>
 
 
 struct User {
@@ -6,3 +8,12 @@ struct User {
 	std::string salt;
 	std::string passwordHash;
 };
+
+using UsersMap = std::unordered_map<std::string, User>;
+
+std::string generateSalt(const std::string &username);
+std::string hashPassword(const std::string &salt, const std::string &password);
+
+bool registerAccount(const UsersMap& users, const std::string &username, const std::string &password);
+bool loginAccount(const UsersMap& users, const std::string &username, const std::string &password);
+
